76d1cba9192f96d275997f8ea156c09b277c87c6
Gabriel Pires <gabpiresdev@gmail.com>
2022-05-18 19:07:19 -0300

    feat: authentication method with github on next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,23 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
 export const SignInButton = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  console.log(session);
+
+  return session ? (
+    <button className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
       Gabriel Pires
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub color="#eba417" />
       Sign In with Github
     </button>
