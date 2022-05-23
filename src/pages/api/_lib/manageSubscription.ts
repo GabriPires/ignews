aa5136363b3e2cf3b719dfe4ccc796f516b0f72d
Gabriel Pires <gabpiresdev@gmail.com>
2022-05-23 20:41:25 -0300

    feat: listen more events and updating a existing subscription when it has changed

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,6 +5,7 @@ import { stripe } from '../../../services/stripe';
 export const saveSubscription = async (
   subscriptionId: string,
   customerId: string,
+  createAction = false,
 ) => {
   const userRef = await fauna.query(
     q.Select(
@@ -22,9 +23,23 @@ export const saveSubscription = async (
     price_id: subscription.items.data[0].price.id,
   };
 
-  await fauna.query(
-    q.Create('subscriptions', {
-      data: subscriptionData,
-    }),
-  );
+  if (createAction) {
+    await fauna.query(
+      q.Create('subscriptions', {
+        data: subscriptionData,
+      }),
+    );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          'ref',
+          q.Get(q.Match(q.Index('subscription_by_id'), subscriptionId)),
+        ),
+        {
+          data: subscriptionData,
+        },
+      ),
+    );
+  }
 };
